f50edf3d61a3a3a53e217cb96cd845f0c4990e30
wulfy <wulfy72@yahoo.Fr>
2014-01-07 02:03:52 +0100

    modification du tri angular pour prendre en compte les timestamp

--- js/controllers.js
+++ js/controllers.js
@@ -7,7 +7,7 @@ var phonecatApp = angular.module('coffrefortApp', []);
 phonecatApp.controller('FacturesListCtrl', ['$scope', '$http', function($scope, $http) {
 
  $scope.directories = angularData;
-  $scope.orderProp = 'date';
+  $scope.orderProp = 'timestamp';
 
 }]);
 
